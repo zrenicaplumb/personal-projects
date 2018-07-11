0e524ba1af2d795b90a75a308123c223dcc54858
Kosmic Kitty <12849002+zrenicaplumb@users.noreply.github.com>
2018-07-10 20:34:06 -0600

    Add files via upload

---
+++ 07.09.2018/index.php
@@ -0,0 +1,159 @@
+<!DOCTYPE html>
+<html lang="en">
+	<head>
+		<meta charset="UTF-8">
+		
+		<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+		
+		<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+
+		
+		<link href='https://fonts.googleapis.com/css?family=Lato:400,700' rel='stylesheet' type='text/css'>
+		<link href="css/reset.css" rel="stylesheet">
+		<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/css/bootstrap.min.css">
+		<link href="css/main.css" rel="stylesheet"> 
+		<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.1/js/bootstrap.min.js" type="text/javascript"></script>
+		<script src="js/main.js" type="text/javascript"></script>
+
+		<title>Php/Js practice</title>
+	</head>
+
+	<body>
+
+		<section class="one">
+			<header>
+				<a href="index.php"><img src="img/logo.png" id="logo"></a>
+				<nav>			
+					<ul>
+						<li>
+							<a href="">Work</a>
+						</li>
+						<li>
+							<a href="">About</a>
+						</li>
+						<li>
+							<a href="">Contact</a>
+						</li>
+					</ul>
+				</nav>
+			</header>
+			<main>
+				<div class="container">
+					<article>
+						<h1 class="greeting-text">Lorem ipsum dolor sit amet.</h1>
+						<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+					</article>
+					<article>
+						<div class="row">	
+							<div class="col-xs-12 col-sm-6">
+								<img src="img/pic1.jpeg">
+								<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+							</div>
+							<div class="col-xs-12 col-sm-6">
+								<p>"Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum."</p>
+							</div>
+						</div>
+					</article>
+				</div>
+			</main>
+		</section>
+		<section class="two">
+			<div class="container">
+				<h2>Add your favorites</h2>
+				<div id="accordion">
+				  	<div class="card">
+				    	<div class="card-header" id="headingOne">
+					      	<h5 class="mb-0">
+						        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseOne" aria-expanded="true" aria-controls="collapseOne">
+						          Music
+						        </button>
+					      	</h5>
+				    	</div>
+				    	<div id="collapseOne" class="collapse hide" aria-labelledby="headingOne" data-parent="#accordion">
+					      	<div class="card-body">
+					        	Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					      	</div>
+				    	</div>
+				  	</div>
+
+				  	<div class="card">
+				    	<div class="card-header" id="headingTwo">
+				      		<h5 class="mb-0">
+					        	<button class="btn btn-link" data-toggle="collapse" data-target="#collapseTwo" aria-expanded="true" aria-controls="collapseTwo">
+					          Books
+					        	</button>
+				      		</h5>
+				    	</div>
+
+				    	<div id="collapseTwo" class="collapse hide" aria-labelledby="headingTwo" data-parent="#accordion">
+						    <div class="card-body">
+						        Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+						    </div>
+				    	</div>
+				  	</div>
+
+				  	<div class="card">
+				    	<div class="card-header" id="headingThree">
+					      	<h5 class="mb-0">
+						        <button class="btn btn-link" data-toggle="collapse" data-target="#collapseThree" aria-expanded="true" aria-controls="collapseThree">
+						          Movies
+						        </button>
+					      	</h5>
+				    	</div>
+
+				    	<div id="collapseThree" class="collapse hide" aria-labelledby="headingThree" data-parent="#accordion">
+					      	<div class="card-body">
+					        	Anim pariatur cliche reprehenderit, enim eiusmod high life accusamus terry richardson ad squid. 3 wolf moon officia aute, non cupidatat skateboard dolor brunch. Food truck quinoa nesciunt laborum eiusmod. Brunch 3 wolf moon tempor, sunt aliqua put a bird on it squid single-origin coffee nulla assumenda shoreditch et. Nihil anim keffiyeh helvetica, craft beer labore wes anderson cred nesciunt sapiente ea proident. Ad vegan excepteur butcher vice lomo. Leggings occaecat craft beer farm-to-table, raw denim aesthetic synth nesciunt you probably haven't heard of them accusamus labore sustainable VHS.
+					      	</div>
+				    	</div>
+				  	</div>
+				</div>
+			</div>	
+		</section>
+		<section class="three">
+		
+				<div class="container">
+					
+					<h3>To add a new item, select a media type.</h3>
+					<div class="media-btn-wrap">
+						<button id="music-btn">Music</button>
+						<button id="book-btn">Books</button>
+						<button id="movie-btn">Movies</button>
+					</div>
+					<form method="post" action="info.php" class="hidden music-form">
+						<h4>Music</h4>
+						<input type="text" name="genre" placeholder="Genre">
+						<input type="text" name="artist" placeholder="Artist">
+						<input type="file" name="album_art" placeholder="Album cover/artist">
+						<button class="btn" type="submit" name="submit">Add to music</button>
+					</form>	
+					<form method="post" action="info.php" class="hidden book-form">
+						<h4>Book</h4>
+						<input type="text" name="genre" placeholder="Genre">
+						<input type="text" name="author" placeholder="Author">
+						<input type="file" name="cover_art" placeholder="Book cover">
+						<button class="btn" type="submit" name="submit">Add to books</button>
+					</form>	
+					<form method="post" action="info.php" class="hidden movie-form">
+						<h4>Movie</h4>
+						<input type="text" name="genre" placeholder="Genre">
+						<input type="text" name="title" placeholder="Title">
+						<input type="file" name="movie_art" placeholder="Movie art">
+						<button class="btn" type="submit" name="submit">Add to movies</button>
+					</form>	
+						
+				</div>
+			
+		</section>
+		<section class="four">
+			<div class="container">
+				<form method="post" action="info.php">
+					<input type="text" name="firstname" placeholder="First name">
+					<input type="text" name="lastname" placeholder="Last name">
+					<input type="email" name="email" placeholder="Email">
+					<button class="btn info-btn" type="submit" name="submit">Submit</button>
+				</form>
+			</div>
+		</section>
+	</body>
+</html>
