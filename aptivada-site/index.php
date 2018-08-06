64ea461c4edc410246c236966fc0ac1ee51d19fa
zrenicaplumb <zrenicaplumb@gmail.com>
2018-08-06 12:44:52 -0600

    skeleton
    skeleton of site

---
+++ aptivada-site/index.php
@@ -0,0 +1,244 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="UTF-8">	
+	<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+	<title>Home Page</title>
+	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
+	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js">
+	<link rel="stylesheet" type="text/css" href="css/main.css">
+	<script src="js/main.js" type="text/javascript"></script>
+</head>
+<body>
+	<div class="top-bar">
+		<div class="container">
+			<div class="topbar-left">
+				<a href="index.php">
+					<img src="logo.jpg" id="logo">
+				</a>
+			</div>
+			<div class="topbar-right">
+				<nav>
+					<ul>
+						<li class="nav-item">
+							<a href="">Filler</a>
+							<div class="nav-submenu">
+								
+							</div>
+						</li>
+						<li class="nav-item">
+							<a href="">Filler</a>
+						</li>
+						<li class="nav-item">
+							<a href="">Filler</a>
+						</li>
+						<li class="nav-item">
+							<a href="">Filler</a>
+						</li>
+					</ul>
+					<div class="sign-in-wrap">
+						<a class="sign-in-btn btn">SIGN IN</a>
+					</div>
+				</nav>
+			</div>
+		</div>
+	</div>
+	<header>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-md-6">
+					<h1>Intro heading woohoo</h1>
+					<h4>Interesting text, yayyyy</h4>
+					<a href="#" class="btn">Get Started</a>
+				</div>
+			</div>
+		</div>
+	</header>
+	<main class="main-content">
+
+		<!-- section with row of icons -->
+		<section class="section">
+			<div class="container">
+				<div class="section-header">
+					<small>Lorem Ipsum</small>
+					<h2>Heading</h2>
+					<hr>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. .</p>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-sm-4 col-md-3">
+						<div class="card">
+							<a class="card-block">
+								<p>
+									<i>Icon goes here</i>
+								</p>
+								<h5 class="card-title">Icon title</h5>
+							</a>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-4 col-md-3">
+						<div class="card">
+							<a class="card-block">
+								<p>
+									<i>Icon goes here</i>
+								</p>
+								<h5 class="card-title">Icon title</h5>
+							</a>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-4 col-md-3">
+						<div class="card">
+							<a class="card-block">
+								<p>
+									<i>Icon goes here</i>
+								</p>
+								<h5 class="card-title">Icon title</h5>
+							</a>
+						</div>
+					</div>
+					<div class="col-xs-12 col-sm-4 col-md-3">
+						<div class="card">
+							<a class="card-block">
+								<p>
+									<i>Icon goes here</i>
+								</p>
+								<h5 class="card-title">Icon title</h5>
+							</a>
+						</div>
+					</div>
+				</div>
+			</div>
+		</section>
+
+		<!-- section with background image -->
+		<section class="section">
+			<div class="container">
+				<h5>LORE IPSUM LOREM IPSUM LOREM LOREM IPSUM <strong>IPSUM?</strong></h5>
+				<p>
+					<a href="#">Click me</a>
+				</p>
+			</div>
+		</section>
+
+		<section class="section">
+			<div class="container">
+				<div class="section-header">
+					<small>Lorem Ipsum</small>
+					<h2>Heading</h2>
+					<hr>
+					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-md-5">
+						<img src="" alt="gif">
+					</div>
+					<div class="col-xs-12 col-md-7">
+						<h4>Smart Pitch</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<a href="#">Learn More</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-md-7">
+						<h4>Smart Pitch</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<a href="#">Learn More</a>
+					</div>
+					<div class="col-xs-12 col-md-5">
+						<img src="" alt="gif">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-md-5">
+						<img src="" alt="gif">
+					</div>
+					<div class="col-xs-12 col-md-7">
+						<h4>Smart Pitch</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<a href="#">Learn More</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-md-7">
+						<h4>Smart Pitch</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<a href="#">Learn More</a>
+					</div>
+					<div class="col-xs-12 col-md-5">
+						<img src="" alt="gif">
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-md-5">
+						<img src="" alt="gif">
+					</div>
+					<div class="col-xs-12 col-md-7">
+						<h4>Smart Pitch</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<a href="#">Learn More</a>
+					</div>
+				</div>
+				<div class="row">
+					<div class="col-xs-12 col-md-7">
+						<h4>Smart Pitch</h4>
+						<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.</p>
+						<a href="#">Learn More</a>
+					</div>
+					<div class="col-xs-12 col-md-5">
+						<img src="" alt="gif">
+					</div>
+				</div>
+
+			</div>
+		</section>
+
+		<section class="section">
+			<div class="container">
+				<h3><strong>Join</strong> thousands of companies that use Aptivada globally</h3>
+				<div class="carousel">
+					<div class="carousel-wrap">
+						
+					</div>
+				</div>
+			</div>
+		</section>
+	</main>
+	<footer>
+		<div class="container">
+			<div class="row">
+				<div class="col-xs-12 col-md-6">
+					<p>Copyright 2018</p>
+				</div>
+				<div class="col-xs-12 col-md-6">
+					<ul>
+						<li>
+							<a href="">Terms</a>
+						</li>
+						<li>
+							<a href="">Privacy Policy</a>
+						</li>
+						<li>
+							<a href="">About</a>
+						</li>
+						<li class="col-xs-12 col-lg-3">
+							<div class="social">
+								<a href="">
+									<i>fb icon</i>
+								</a>
+								<a href="">
+									<i>twitter icon</i>
+								</a>
+								<a href="">
+									<i>instagram icon</i>
+								</a>
+							</div>
+						</li>
+					</ul>
+				</div>
+			</div>
+		</div>
+	</footer>
+</body>
+</html>
