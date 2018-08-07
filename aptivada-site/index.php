8d564b18b27ddf1d0db91b98965d6538e33893be
zrenicaplumb <zrenicaplumb@gmail.com>
2018-08-07 16:20:22 -0600

    homepage changes
    homepage mostly finished

--- aptivada-site/index.php
+++ aptivada-site/index.php
@@ -8,43 +8,82 @@
 	<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.9.1/jquery.min.js"></script>
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css">
 	<link rel="stylesheet" type="text/css" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js">
+	<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
 	<link rel="stylesheet" type="text/css" href="css/main.css">
 	<script src="js/main.js" type="text/javascript"></script>
 </head>
 <body>
+	
 	<div class="top-bar">
 		<div class="container">
 			<div class="top-bar-left">
 				<a href="index.php">
-					<img src="../img/fakelogo.png" id="logo">
+					<img src="img/logo.png" id="logo">
 				</a>
 			</div>
 			<div class="top-bar-right">
 				<nav>
 					<ul>
-						<li class="nav-item">
-							<a href="">Filler</a>
+						<li class="nav-item nav-item-1">
+							<a href="">PRODUCTS<i class="fa fa-caret-down"></i></a>
 							<div class="nav-submenu">
-								
+								<div class="product-container">
+									<div>
+										<a href="">
+											<img src="img/icon1.png">
+											<h5>Smart Pitch</h5>
+										</a>
+									</div>
+									<div>
+										<a href="">
+											<img src="img/icon2.png">
+											<h5>Smart Pitch</h5>
+										</a>
+									</div>
+									<div>
+										<a href="">
+											<img src="img/icon3.png">
+											<h5>Smart Pitch</h5>
+										</a>
+									</div>
+									<div>
+										<a href="">
+											<img src="img/icon4.png">
+											<h5>Smart Pitch</h5>
+										</a>
+									</div>
+									<div>
+										<a href="">
+											<img src="img/icon5.png">
+											<h5>Smart Pitch</h5>
+										</a>
+									</div>
+									<div>
+										<a href="">
+											<img src="img/icon6.png">
+											<h5>Smart Pitch</h5>
+										</a>
+									</div>
+								</div>
 							</div>
 						</li>
 						<li class="nav-item">
-							<a href="">Filler</a>
+							<a href="">NEWS</a>
 						</li>
 						<li class="nav-item">
-							<a href="">Filler</a>
+							<a href="">ABOUT</a>
 						</li>
-						<li class="nav-item">
-							<a href="">Filler</a>
-						</li>
+						
+						
 					</ul>
 					<div class="sign-in-wrap">
-						<a class="sign-in-btn btn">SIGN IN</a>
+						<a class="sign-in-btn btn ">SIGN IN</a>
 					</div>
 				</nav>
 			</div>
 		</div>
 	</div>
+
 	<header>
 		<div class="container">
 			<div class="row">
@@ -65,10 +104,10 @@
 		<section class="section section-one">
 			<div class="container">
 				<div class="section-header">
-					<small>Lorem Ipsum</small>
-					<h2>Heading</h2>
+					<small>PRODUCTS</small>
+					<h2>Everything you need to stand out</h2>
 					<hr>
-					<p>Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. .</p>
+					<p>We build smart products for brand communications which convert to increased visibility, better relationships and more sales.</p>
 				</div>
 				<div class="row">
 					<div class="col-xs-12 col-sm-4 col-md-3">
@@ -218,29 +257,32 @@
 		<div class="container">
 			<div class="row">
 				<div class="col-xs-12 col-md-6">
-					<p>Copyright 2018</p>
+					<p>Copyright © 2018 Aptivada, Inc. | All rights reserved.</p>
 				</div>
 				<div class="col-xs-12 col-md-6">
+
 					<ul>
-						<li>
+						<li class="nav-link">
 							<a href="">Terms</a>
 						</li>
-						<li>
+						<li class="nav-link">
 							<a href="">Privacy Policy</a>
 						</li>
-						<li>
+						<li class="nav-link">
 							<a href="">About</a>
 						</li>
 						<li class="col-xs-12 col-lg-3">
 							<div class="social">
 								<a href="">
-									<i>fb icon</i>
+									<i class="fa fa-facebook-f"></i>
+
+
 								</a>
 								<a href="">
-									<i>twitter icon</i>
+									<i class="fa fa-twitter"></i>
 								</a>
 								<a href="">
-									<i>instagram icon</i>
+									<i class="fa fa-instagram"></i>
 								</a>
 							</div>
 						</li>
